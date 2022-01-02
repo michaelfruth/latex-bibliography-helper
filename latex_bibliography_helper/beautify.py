75b9af1ed941077040942e5942f2be06aacc4f06
Michael Fruth <Michael.Fruth@uni-passau.de>
2022-01-02 15:38:52 +0100

    Refactored beautify to use new modules (refactor)

--- latex_bibliography_helper/beautify.py
+++ latex_bibliography_helper/beautify.py
@@ -1,43 +1,52 @@
+from bibtexparser.bwriter import BibTexWriter
 import logging
+from handler import bibtex_handler
 
 import bibtexparser
 
-import bib_util
+import util
+import config
 
 logger = logging.getLogger(__name__)
 
 
-def style(bib_database, curlify):
+def style(bib_database, curlify, pretty):
     all_attributes = set()
 
     for bib_entry in bib_database.entries:
         if curlify:
-            bib_util.curlify_title(bib_entry)
+            util.curlify_title(bib_entry)
+        if pretty:
+            util.hide_attributes(bib_entry)
+            if config.is_rewrite_booktitle():
+                util.rewrite_booktitle(bib_entry)
 
-        bib_util.hide_attributes(bib_entry)
+        util.hide_attributes(bib_entry)
 
         # Collect all attributes
         all_attributes.update(list(bib_entry.keys()))
 
-    writer = bib_util.get_bibtex_writer()
+    writer = BibTexWriter()
 
-    if bib_util.get_config_property("style", "sort"):
-        attributes_order = bib_util.get_attributes_order()
-        bib_util.order_hidden_attributes(all_attributes, attributes_order)
-
-        writer.display_order = attributes_order
+    attributes_order = writer.display_order
+    if config.is_sort_attributes():
+        # Order attributes
+        attributes_order = bibtex_handler.create_attributes_order(all_attributes,
+                                                                  config.get_attribute_names(),
+                                                                  config.get_hide_prefix())
+    writer.display_order = attributes_order
 
     bib = writer.write(bib_database)
     return bib
 
 
-def beautify(content, curlify, copy_to_clipboard):
+def beautify(content, curlify, copy_to_clipboard, pretty):
     parser = bibtexparser.bparser.BibTexParser(ignore_nonstandard_types=False,
                                                common_strings=True)
     bib_database = bibtexparser.loads(content, parser=parser)
 
-    content = style(bib_database, curlify)
+    content = style(bib_database, curlify, pretty)
 
     if copy_to_clipboard:
-        bib_util.copy_to_clipboard(content)
+        util.copy_to_clipboard(content)
     print(content)
