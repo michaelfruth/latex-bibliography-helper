99e3f568e9ab41abdb80162f91635fbc1b4c72ce
Michael Fruth <Michael.Fruth@uni-passau.de>
2022-01-08 23:26:46 +0100

    Fixed import

--- bibhelper/beautify.py
+++ bibhelper/beautify.py
@@ -1,9 +1,9 @@
 import logging
 
 import bibtexparser
-import config
-import util
+from bibhelper.config import is_sort_attributes, is_rewrite_booktitle
 from bibhelper.handler.bibtex_handler import apply_bibtex_writer_style, create_attributes_order
+from bibhelper.util import curlify_title, rewrite_booktitle, get_attribute_names, hide_attributes, get_hide_prefix
 from bibtexparser.bwriter import BibTexWriter
 
 logger = logging.getLogger(__name__)
@@ -14,11 +14,11 @@ def style(bib_database, curlify, pretty):
 
     for bib_entry in bib_database.entries:
         if curlify:
-            util.curlify_title(bib_entry)
+            curlify_title(bib_entry)
         if pretty:
-            util.hide_attributes(bib_entry)
-            if config.is_rewrite_booktitle():
-                util.rewrite_booktitle(bib_entry)
+            hide_attributes(bib_entry)
+            if is_rewrite_booktitle():
+                rewrite_booktitle(bib_entry)
 
         # Collect all attributes
         all_attributes.update(list(bib_entry.keys()))
@@ -27,11 +27,11 @@ def style(bib_database, curlify, pretty):
     apply_bibtex_writer_style(writer)
 
     attributes_order = writer.display_order
-    if config.is_sort_attributes():
+    if is_sort_attributes():
         # Order attributes
         attributes_order = create_attributes_order(all_attributes,
-                                                   config.get_attribute_names(),
-                                                   config.get_hide_prefix())
+                                                   get_attribute_names(),
+                                                   get_hide_prefix())
     writer.display_order = attributes_order
 
     bib = writer.write(bib_database)
@@ -46,5 +46,5 @@ def beautify(content, curlify, copy_to_clipboard, pretty):
     content = style(bib_database, curlify, pretty)
 
     if copy_to_clipboard:
-        util.copy_to_clipboard(content)
+        copy_to_clipboard(content)
     print(content)
